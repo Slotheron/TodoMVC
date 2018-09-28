ddc9ab2b714c15482ccd0e94b2bcd5cdd3d26270
Slotheron <32925757+Slotheron@users.noreply.github.com>
2018-09-28 14:24:51 +0200

    Added javascript function for adding list item

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,1 +1,30 @@
-///
+let inputBox = document.getElementById("add");
+document.addEventListener("keydown", enter);
+
+function enter(e) {
+    if (e.keyCode == '13') {
+        addListElement();
+    }
+}
+
+let itemId = 0;
+function addListElement() {
+    itemId++;
+    let text = document.getElementById("add");
+    let parent = document.getElementById('list');
+    let child = document.createElement('li');
+    let input = document.createElement('input');
+    input.type = 'checkbox';
+    let label = document.createElement('label');
+    label.text = text.value;
+    label.value = text.value;
+    label.textContent = text.value;
+    let button = document.createElement('button');
+    button.textContent = 'X'
+    child.appendChild(input);
+    child.appendChild(label);
+    child.appendChild(button);
+    child.setAttribute('id', itemId);
+    parent.appendChild(child);
+}
+
