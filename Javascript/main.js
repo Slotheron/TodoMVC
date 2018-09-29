1fe717d68d32d986ae9437d3bbfbe055fbf476e3
Slotheron <32925757+Slotheron@users.noreply.github.com>
2018-09-29 14:58:55 +0200

    added validation and text reset on inputs

--- Javascript/main.js
+++ Javascript/main.js
@@ -6,33 +6,49 @@ function enter(e) {
     }
 }
 
+let listCount = 0;
 let itemId = 0;
 function addListElement() {
-    itemId++;
-    let currentId = itemId;
-    let text = document.getElementById("add");
-    let parent = document.getElementById('list');
-    let child = document.createElement('li');
-    let input = document.createElement('input');
-    input.type = 'checkbox';
-    let label = document.createElement('label');
-    label.text = text.value;
-    label.value = text.value;
-    label.textContent = text.value;
-    let button = document.createElement('button');
-    button.setAttribute('id', 'buttonItem' + currentId);
-    button.textContent = 'X'
-    child.appendChild(input);
-    child.appendChild(label);
-    child.appendChild(button);
-    child.setAttribute('id', 'listItem' + currentId);
-    parent.appendChild(child);
-    document.getElementById('buttonItem' + currentId).addEventListener('click',function(){
-        removeListElement('listItem' + currentId);
-    });
+    if (validate()) {
+        itemId++;
+        let currentId = itemId;
+        let text = document.getElementById("add");
+        let parent = document.getElementById('list');
+        let child = document.createElement('li');
+        let input = document.createElement('input');
+        input.type = 'checkbox';
+        let label = document.createElement('label');
+        label.text = text.value;
+        label.value = text.value;
+        label.textContent = text.value;
+        let button = document.createElement('button');
+        button.setAttribute('id', 'buttonItem' + currentId);
+        button.textContent = 'X'
+        child.appendChild(input);
+        child.appendChild(label);
+        child.appendChild(button);
+        child.setAttribute('id', 'listItem' + currentId);
+        parent.appendChild(child);
+        document.getElementById('buttonItem' + currentId).addEventListener('click', function () {
+            removeListElement('listItem' + currentId);
+        });
+        listCount++;
+        text.value = "";
+    }
 }
-function removeListElement(listItemId){
+function removeListElement(listItemId) {
     let parent = document.getElementById('list');
     let element = document.getElementById(listItemId);
     parent.removeChild(element);
+    listCount--;
+}
+
+function validate() {
+    let text = document.getElementById("add");
+    if (text.value == "" || text.value == null) {
+        return false;
+    }
+    else {
+        return true;
+    }
 }
